96e3a78cc138cdb16b1b9b07a54d59698685dc0c
impulse-star <not@real.email>
2023-08-13 01:55:37 -0600

    Created error function.

--- website/js/index.js
+++ website/js/index.js
@@ -1,3 +1,13 @@
 document.getElementById('noJSWarningMessage').remove();
 
+function raiseError(error) {
+    alert(`An error has occured.\nThe website may no longer be functional.\nError: ${error}`);
+}
+
+// fetch().then(res => {
+
+// }).catch(err => {
+//     raiseError(err);
+// });
+
 console.log("Loaded");
