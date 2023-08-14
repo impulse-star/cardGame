69fc2b08f71562ed4895ad78a52c73121a251576
impulse-star <not@real.email>
2023-08-13 19:36:22 -0600

    fixed an issue with the code not running

--- gameWebServer/main.py
+++ gameWebServer/main.py
@@ -2,6 +2,8 @@ import sys
 
 
 if not sys.prefix != sys.base_prefix:
+    print("Error, program started with non virtual environment python." +
+          "please create a virtual environment first.")
     exit(1)
 
 
@@ -9,5 +11,5 @@ def main():
     print('Hello world')
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
