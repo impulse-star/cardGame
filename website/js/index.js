c6f5169c889e6375414c9bcb552675285be0dbec
impulse-star <not@real.email>
2023-08-13 19:07:16 -0600

    added redirect utility function

--- website/js/index.js
+++ website/js/index.js
@@ -1,7 +1,12 @@
 document.getElementById('noJSWarningMessage').remove();
 
+function redirectTo(url) {
+    window.location.href = url;
+}
 function raiseError(error) {
-    alert(`An error has occured.\nThe website may no longer be functional.\nError: ${error}`);
+    alert(`An error has occured.\nThe website may no longer be functional.` +
+    `\nPlease reload the page. If the error persists, contact the website creator.` +
+    `\nError: ${error}`);
 }
 
 // fetch().then(res => {
